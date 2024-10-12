f8fefe1d80c67a7f1b261596ae083f6eae5a2dd1
Tiffany Liu <mazingmonkey2004@gmail.com>
2024-10-11 22:40:28 -0400

    hopefully set up backend

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+// index.js hopefully base code
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
