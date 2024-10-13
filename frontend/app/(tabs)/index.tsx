7b2c66238d14ead9c1453edf6855e57a97c89ac5
Wyatt <wnapier@bu.edu>
2024-10-12 23:03:26 -0400

    styling the upload recipe (index) page

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -78,10 +78,20 @@ export default function HomeScreen() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Add Image:</Text>
+      <Text style={[styles.header1, { transform: [{ translateX: 0.5 }, { translateY: 1}], color: "#E07BE0"}]}>
+        WELCOME TO B.T.R.
+      </Text>
+      <Text style={styles.header1}>
+        WELCOME TO B.T.R.
+      </Text>
 
+      <Text style={[styles.header, { height: file ? 400 : 200, marginBottom: file ? -340 : -120 }]}>
+        Add Image:
+      </Text>
+
+ 
       {/* Button to choose an image */}
-      <TouchableOpacity style={styles.button} onPress={pickImage}>
+      <TouchableOpacity style={[styles.button, { display: file ? "none" : "flex" }]} onPress={pickImage}>
         <Text style={styles.buttonText}>Choose Image</Text>
       </TouchableOpacity>
 
@@ -106,10 +116,26 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     padding: 16,
+    backgroundColor: "#CEFFCD",
+    borderRadius: 8,
   },
   header: {
     fontSize: 20,
-    marginBottom: 16,
+    textAlign: "center",
+    paddingTop: 16,
+    marginBottom: -130,
+    width: 300,
+    height: 400,
+    backgroundColor: "#E07BE0",
+    fontWeight: "500",
+    borderRadius: 8,
+  },
+  header1: {
+    fontSize: 20,
+    marginTop: -60,
+    marginBottom: 40,
+    fontWeight: "1000",
+    color: "#0A1045",
   },
   button: {
     backgroundColor: "#007AFF",
@@ -138,7 +164,7 @@ const styles = StyleSheet.create({
   },
   image: {
     width: 200,
-    height: 200,
+    height: 300,
     borderRadius: 8,
   },
   errorText: {
